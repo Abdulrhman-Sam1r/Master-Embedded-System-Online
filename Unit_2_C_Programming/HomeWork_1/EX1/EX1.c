f203900625e9472e357e7d85cc08b3e18288a7ac
Abdulrhman Samir <abdulrhman.2.samir@gmail.com>
2024-06-26 19:41:10 +0300

    Just started doing the HW_1 Finished EX1 & EX2

---
+++ Unit_2_C_Programming/HomeWork_1/EX1/EX1.c
@@ -0,0 +1,13 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Jun 26, 2024
+ *      Author: MSI PC
+ */
+
+#include <stdio.h>
+
+void main()
+{
+	printf("C Programming");
+}
---
+++ Unit_2_C_Programming/HomeWork_1/EX2/EX2.c
@@ -0,0 +1,18 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Jun 26, 2024
+ *      Author: MSI PC
+ */
+
+
+#include <stdio.h>
+
+void main()
+{
+	int x = 0 ;
+	printf("Enter an integer: ");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d\n",x);
+}
